de2bb7f87097cac34f246fc892160b1cda18ba68
1309400 <1309400@RG43015.rgu.ac.uk>
2015-02-23 09:52:50 +0000

    Change @9:52

--- src/week4/Address.java
+++ src/week4/Address.java
@@ -10,13 +10,15 @@ package week4;
 public class Address {
     private String emailAddress;
     private String city;
+    private String postCode;
     
     /**
      * 
      * @param emailAddress email address
      */
-    public Address(String city, String emailAddress) {
+    public Address(String city, String emailAddress, String postCode) {
         this.emailAddress = emailAddress;
         this.city = city;
+        this.postCode = postCode;
     }
 }
